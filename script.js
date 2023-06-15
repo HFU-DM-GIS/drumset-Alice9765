726116a4ad01fa63ea9c42ee698bc62e35856ce1
benitqpham <benitqpham@gmail.com>
2023-06-15 14:08:48 +0200

    current

--- script.js
+++ script.js
@@ -1,9 +1,97 @@
+const keySounds = {
+  'w': "sounds/tom-1.mp3",
+  'a': "sounds/tom-2.mp3",
+  's': 'sounds/tom-3.mp3',
+  'd': 'sounds/tom-4.mp3',
+  'j': 'sounds/snare.mp3',
+  'k': 'sounds/crash.mp3',
+  'l': 'sounds/kick-bass.mp3',
+  'i': 'sounds/kick2.mp3'
+}
+
+let rec = false;
+
+let play = false;
+
+let recSounds = []
+
+let soundsPlay = []
+
+const recButton = document.querySelector(".rec")
+
+const delButton = document.querySelector(".del")
+
+const playButton = document.querySelector(".play")
+
+recButton.addEventListener("click", function () {
+  if (rec) {
+    rec = false
+    recButton.classList.remove("active")
+    console.log(recSounds)
+  } else {
+    rec = true
+    recButton.classList.add("active")
+    UselessFact();
+  }
+});
+
+delButton.addEventListener("click", function () {
+  recSounds = []
+})
+
+function selectElementsBySrc(selector) {
+  const elements = document.querySelectorAll(selector);
+  const selectedElements = Array.from(elements).filter(element => {
+    return element.getAttribute('src');
+  });
+  return selectedElements;
+}
+
+playButton.addEventListener("click", function (e) {
+  let icon = document.querySelector(".play .fa")
+
+  if (!play) {
+    icon.classList.remove("fa-play")
+    icon.classList.add("fa-pause")
+    play = true
+    playRec()
+  } else {
+    icon.classList.remove("fa-pause") 
+    icon.classList.add("fa-play")
+    play = false
+  }
+})
+
+function playRec() {
+  let bpm = document.querySelector('#bpm').value
+  let milSecs = 60/bpm*1000
+  console.log(milSecs)
+  if (play) {
+    recSounds.forEach((soundKey, i) => {
+    let path = keySounds[soundKey]
+    let sound = new Audio(path)
+    soundsPlay.push(sound)
+    setTimeout(() => {
+      sound.play()
+      console.log(i, recSounds.length)
+      if (i+1 == recSounds.length) {
+        console.log('end')
+        soundsPlay = []
+        setTimeout(() => {
+          playRec()
+        }, milSecs)
+      }
+    }, i * milSecs)
+  });
+  }
+  
+}
 
 const drumButtons = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drumButtons.length; i++) {
 
-    drumButtons[i].addEventListener("click", function() {
+  drumButtons[i].addEventListener("click", function () {
 
     const buttonInnerHTML = this.innerHTML;
 
@@ -11,82 +99,70 @@ for (let i = 0; i < drumButtons.length; i++) {
 
     buttonAnimation(buttonInnerHTML);
 
-
+    if (rec) {
+      console.log(buttonInnerHTML)
+    }
 
   });
 
 }
 
-document.addEventListener("keydown", function(event) { // keydown removes delay
-  if (!event.repeat)  { // removes sound spamming
+document.addEventListener("keydown", function (event) { // keydown removes delay
+  if (!event.repeat) { // removes sound spamming
     makeSound(event.key);
-  
+
     buttonAnimation(event.key);
+
   }
-  
-  });
 
-  document.addEventListener("keyup", function(event) {
-    const currentKey = event.key;
-    const activeButton = document.querySelector("." + currentKey);
+});
+
+document.addEventListener("keyup", function (event) {
+  const currentKey = event.key;
+  let activeButton = null
+  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j'  || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
+    activeButton = document.querySelector("." + currentKey);
+  }
+  if (activeButton !== null) {
     activeButton.classList.remove("pressed"); // removes "pressed" style after keyup event
-  
-  });
+  }
 
+});
 
 function makeSound(key) {
 
-  switch (key) {
-    case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
+  let path = keySounds[key]
 
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
+  if (path) {
+    new Audio(path).play();
 
-    case "s":
-      let tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
-      break;
+    if (rec) {
+      recSounds.push(key)
+    }
 
-    case "d":
-      let tom4 = new Audio('sounds/tom-4.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      let snare = new Audio('sounds/snare.mp3');
-      snare.play();
-      break;
-
-    case "k":
-      let crash = new Audio('sounds/crash.mp3');
-      crash.play();
-      break;
-
-    case "l":
-      let kick = new Audio('sounds/kick-bass.mp3');
-      kick.play();
-      break;
-
-    case "i":
-      let kick2 = new Audio('sounds/kick2.mp3');
-      kick2.play();
-      break;
-
-    default: console.log(key);
 
   }
+
+}
+
+function UselessFact() {
+  fetch('https://uselessfacts.jsph.pl/random.json?language=de')
+    .then(response => response.json())
+    .then(data => {
+      const fact = data.text;
+      document.getElementById('fact').textContent = fact;
+    })
 }
 
 
 function buttonAnimation(currentKey) {
 
-  let activeButton = document.querySelector("." + currentKey);
-
-  activeButton.classList.add("pressed");
+  let activeButton = null
+  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j'  || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
+    activeButton = document.querySelector("." + currentKey);
+  }
+  if (activeButton !== null) {
+    activeButton.classList.add("pressed");
+  }
 }
 
