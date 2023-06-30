f02404574c5afaf2caa9fb6f3b76770f64de0707
benitqpham <benitqpham@gmail.com>
2023-06-30 11:52:39 +0200

    changes: - del button removes entire recording list - bpm input for recording list

--- script.js
+++ script.js
@@ -7,147 +7,139 @@ const keySounds = {
   'k': 'sounds/crash.mp3',
   'l': 'sounds/kick-bass.mp3',
   'i': 'sounds/kick2.mp3'
-}
+};
 
 let rec = false;
-
 let play = false;
+let recSounds = [];
+let soundsPlay = [];
 
-let recSounds = []
-
-let soundsPlay = []
-
-const recButton = document.querySelector(".rec")
-
-const delButton = document.querySelector(".del")
-
-const playButton = document.querySelector(".play")
+const recButton = document.querySelector(".rec");
+const delButton = document.querySelector(".del");
+const playButton = document.querySelector(".play");
+const recordingsList = document.getElementById("recordingsList");
+const bpmInput = document.getElementById("bpm");
 
 let recordedSongs = [];
 
+const storedArrayString = localStorage.getItem('recordedSongs');
+
+// Array als JSON in einen String umwandeln
+const arrayString = JSON.stringify(recordedSongs);
+
+// Überprüfen, ob ein gespeichertes Array vorhanden ist
+if (storedArrayString) {
+  // JSON-String in ein Array umwandeln
+  recordedSongs = JSON.parse(storedArrayString);
+  displayRecordings();  
+} else {
+  console.log('Kein gespeichertes Array gefunden');
+}
+
 recButton.addEventListener("click", function () {
   if (rec) {
-    rec = false
-    recButton.classList.remove("active")
-    console.log(recSounds)
+    rec = false;
+    recButton.classList.remove("active");
+    console.log(recSounds);
     recordedSongs.push(recSounds.slice());
     displayRecordings();
     recSounds = [];
+    saveRecordedSongs();
   } else {
-    rec = true
-    recButton.classList.add("active")
+    rec = true;
+    recButton.classList.add("active");
     UselessFact();
   }
 });
 
 delButton.addEventListener("click", function () {
-  recSounds = []
-})
+  recordedSongs = [];
+  saveRecordedSongs();
+  displayRecordings();
+});
 
-function selectElementsBySrc(selector) {
-  const elements = document.querySelectorAll(selector);
-  const selectedElements = Array.from(elements).filter(element => {
-    return element.getAttribute('src');
-  });
-  return selectedElements;
-}
-
-playButton.addEventListener("click", function (e) {
-  let icon = document.querySelector(".play .fa")
-
+playButton.addEventListener("click", function () {
   if (!play) {
-    icon.classList.remove("fa-play")
-    icon.classList.add("fa-pause")
-    play = true
-    playRec()
+    play = true;
+    playButton.classList.add("active");
+    playRec();
   } else {
-    icon.classList.remove("fa-pause") 
-    icon.classList.add("fa-play")
-    play = false
+    play = false;
+    playButton.classList.remove("active");
+    stopPlayback();
   }
-})
+});
 
 function playRec() {
-  let bpm = document.querySelector('#bpm').value
-  let milSecs = 60/bpm*1000
-  console.log(milSecs)
-  if (play) {
-    recSounds.forEach((soundKey, i) => {
-    let path = keySounds[soundKey]
-    let sound = new Audio(path)
-    soundsPlay.push(sound)
+  const bpm = parseInt(bpmInput.value);
+  const milSecs = 60 / bpm * 1000;
+
+  recSounds.forEach((soundKey, i) => {
+    const path = keySounds[soundKey];
+    const sound = new Audio(path);
+    soundsPlay.push(sound);
+
     setTimeout(() => {
-      sound.play()
-      console.log(i, recSounds.length)
-      if (i+1 == recSounds.length) {
-        console.log('end')
-        soundsPlay = []
+      sound.play();
+      console.log(i, recSounds.length);
+
+      if (i + 1 === recSounds.length) {
+        console.log('end');
+        soundsPlay = [];
         setTimeout(() => {
-          playRec()
-        }, milSecs)
+          if (play) {
+            playRec();
+          }
+        }, milSecs);
       }
-    }, i * milSecs)
+    }, i * milSecs);
   });
-  }
-  
 }
 
 const drumButtons = document.querySelectorAll(".drum");
 
-for (let i = 0; i < drumButtons.length; i++) {
-
-  drumButtons[i].addEventListener("click", function () {
-
+drumButtons.forEach((button) => {
+  button.addEventListener("click", function () {
     const buttonInnerHTML = this.innerHTML;
-
     makeSound(buttonInnerHTML);
-
     buttonAnimation(buttonInnerHTML);
 
     if (rec) {
-      console.log(buttonInnerHTML)
+      console.log(buttonInnerHTML);
     }
-
   });
+});
 
-}
-
-document.addEventListener("keydown", function (event) { // keydown removes delay
-  if (!event.repeat) { // removes sound spamming
+document.addEventListener("keydown", function (event) {
+  if (!event.repeat) {
     makeSound(event.key);
-
     buttonAnimation(event.key);
-
   }
-
 });
 
 document.addEventListener("keyup", function (event) {
   const currentKey = event.key;
-  let activeButton = null
-  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j'  || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
+  let activeButton = null;
+
+  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j' || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
     activeButton = document.querySelector("." + currentKey);
   }
-  if (activeButton !== null) {
-    activeButton.classList.remove("pressed"); // removes "pressed" style after keyup event
-  }
 
+  if (activeButton !== null) {
+    activeButton.classList.remove("pressed");
+  }
 });
 
 function makeSound(key) {
-
-  let path = keySounds[key]
+  const path = keySounds[key];
 
   if (path) {
     new Audio(path).play();
 
     if (rec) {
-      recSounds.push(key)
+      recSounds.push(key);
     }
-
-
   }
-
 }
 
 function UselessFact() {
@@ -156,25 +148,24 @@ function UselessFact() {
     .then(data => {
       const fact = data.text;
       document.getElementById('fact').textContent = fact;
-    })
+    });
 }
 
-
 function buttonAnimation(currentKey) {
+  let activeButton = null;
 
-  let activeButton = null
-  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j'  || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
+  if (currentKey === 'a' || currentKey === 'w' || currentKey === 's' || currentKey === 'd' || currentKey === 'j' || currentKey === 'i' || currentKey === 'k' || currentKey === 'l') {
     activeButton = document.querySelector("." + currentKey);
   }
+
   if (activeButton !== null) {
     activeButton.classList.add("pressed");
   }
 }
 
-const recordingsList = document.getElementById("recordingsList");
-
 function displayRecordings() {
   recordingsList.innerHTML = "";
+
   recordedSongs.forEach((recording, index) => {
     const recordingItem = document.createElement("li");
     recordingItem.innerText = `Aufnahme ${index + 1}`;
@@ -190,77 +181,51 @@ function displayRecordings() {
   });
 }
 
-let recordings = [];
-let mediaRecorders = [];
-
-function startRecording(key) {
-  const mediaRecorder = new MediaRecorder(stream);
-  const chunks = [];
-
-  mediaRecorder.addEventListener('dataavailable', (event) => {
-    chunks.push(event.data);
-  });
-
-  mediaRecorder.addEventListener('stop', () => {
-    const recording = new Blob(chunks, { type: 'audio/webm' });
-    chunks.length = 0;
-    addRecording(key, recording);
-  });
-
-  mediaRecorders.push(mediaRecorder);
-  mediaRecorder.start();
-}
-
-function stopRecording() {
-  if (mediaRecorders.length > 0) {
-    const mediaRecorder = mediaRecorders.pop();
-    mediaRecorder.stop();
-  }
-}
-
 function playRecording(recording) {
   if (soundsPlay.length > 0) {
-    soundsPlay.forEach((sound) => sound.pause());
-    soundsPlay = [];
+    stopPlayback();
   }
 
-  let bpm = document.querySelector('#bpm').value;
-  let milSecs = 60 / bpm * 1000;
+  const bpm = parseInt(bpmInput.value);
+  const milSecs = 60 / bpm * 1000;
+  const totalDuration = recording.length * milSecs;
 
-  let index = 0;
-  const playNextSound = () => {
-    if (index >= recording.length) {
-      stopPlayback();
-      return;
-    }
+  let playTime = 0;
 
-    const soundKey = recording[index];
+  recording.forEach((soundKey, index) => {
     const path = keySounds[soundKey];
     const sound = new Audio(path);
     soundsPlay.push(sound);
 
-    sound.play();
+    setTimeout(() => {
+      sound.play();
+    }, playTime);
 
-    sound.addEventListener('ended', () => {
-      index++;
-      playNextSound();
-    });
-  };
+    playTime += milSecs; // Erhöhen Sie die Wartezeit für den nächsten Ton
+  });
 
-  const stopPlayback = () => {
-    play = false;
-    let icon = document.querySelector(".play .fa");
-    icon.classList.remove("fa-pause");
-    icon.classList.add("fa-play");
-    soundsPlay.forEach((sound) => sound.pause());
-    soundsPlay = [];
-  };
+  setTimeout(() => {
+    stopPlayback();
+  }, totalDuration); // Stoppen Sie die Wiedergabe nach der Gesamtdauer des Recordings
+}
 
-  if (!play) {
-    let icon = document.querySelector(".play .fa");
-    icon.classList.remove("fa-play");
-    icon.classList.add("fa-pause");
-    play = true;
-    playNextSound();
-  }
+
+
+function stopPlayback() {
+  play = false;
+
+  soundsPlay.forEach((sound) => {
+    sound.pause();
+    sound.currentTime = 0;
+  });
+
+  soundsPlay = [];
+}
+
+function saveRecordedSongs() {
+  // Array als JSON in einen String umwandeln
+  const arrayString = JSON.stringify(recordedSongs);
+
+  // Array im Local Storage speichern
+  localStorage.setItem('recordedSongs', arrayString);
 }
